2e720f7aba55d19183b4b22c2436449bfd5c72d9
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-02-24 12:13:39 +0100

    test

--- test/config.py
+++ test/config.py
@@ -1,10 +1,6 @@
-import pandas as pd
-import numpy as np
-
 class Config(object):
-    SECRET_KEY = "UY18980yn88921"
+    HANDLER = []
+    SECRET_KEY = "UY8980yn88921"
     TESTING = True
     DEBUG = True
-
-    FRAME = pd.DataFrame(data=np.random.randn(3, 10), index=["A", "B", "C"])
     TABLE_FORMAT = " table table-striped table-bordered table-hover"
--- test/test_flask.py
+++ test/test_flask.py
@@ -1,5 +1,6 @@
 import unittest
-
+import pandas as pd
+import numpy as np
 
 
 from web.app import app
@@ -7,7 +8,8 @@ from test.config import Config
 
 class FlaskTestCase(unittest.TestCase):
     def setUp(self):
-        self.app = app(Config)
+        frame = pd.DataFrame(data=np.random.randn(3, 10), index=["A", "B", "C"])
+        self.app = app(Config, frame=frame)
         self.app = self.app.test_client()
 
     def tearDown(self):
@@ -18,12 +20,4 @@ class FlaskTestCase(unittest.TestCase):
         self.assertEqual(x.status_code, 200)
 
         x = self.app.get('/index')
-        self.assertEqual(x.status_code, 200)
-
-    def testFrame(self):
-        x = self.app.get('/frame')
-        self.assertEqual(x.status_code, 200)
-
-    def testFramex(self):
-        x = self.app.get('/framex/A')
         self.assertEqual(x.status_code, 200)
