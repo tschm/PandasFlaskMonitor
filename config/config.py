9562f42f3c0f508731448a133e4b3fe4f6dbd04a
thomas.schmelzer <thomas.schmelzer@lobnek.com>
2017-02-24 12:13:05 +0100

    config

--- config/config.py
+++ config/config.py
@@ -1,10 +1,11 @@
-import pandas as pd
-import numpy as np
+import logging
+
 
 class Config(object):
+    logging.basicConfig(level=logging.DEBUG)
+    HANDLER = [logging.StreamHandler()]
     WTF_CSRF_ENABLED = True
     SECRET_KEY = "UY18980yn8892125"
     TESTING = False
     DEBUG = False
     TABLE_FORMAT = " table table-striped table-bordered table-hover"
-    FRAME = pd.DataFrame(data=np.random.randn(3, 10), index=["A", "B", "C"])
